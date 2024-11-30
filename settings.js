344884934ad57bfcb7da8fb7897fb9ce6dd93fe3
Nnalue Kingsley <164803861+Ebukannalue1132@users.noreply.github.com>
2024-11-30 12:27:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
-global.ownername = "Abhishek Suresh"
+global.ownernomer = "2349070810971"
+global.ownername = "DEVIL乂ĐĂŘҜ"
 global.ytname = "YT: Comedy Melody CH"
 global.socialm = "GitHub: DGXeon"
 global.location = "India, Kerala, Idukki"
 
-global.ownernumber = '919074692450'  //creator number
-global.ownername = 'Abhishek Suresh' //owner name
-global.botname = '𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
+global.ownernumber = '2349070810971'  //creator number
+global.ownername = 'DEVIL乂ĐĂŘҜ' //owner name
+global.botname = 'DEVIL乂ĐĂŘҜ-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
 global.packname = '🎯𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃'
